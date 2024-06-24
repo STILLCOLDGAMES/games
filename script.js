9e221f11c9324d04fdb192833d5b706f5f2c89ab
STILL COLD <118074876+STILLCOLDGAMES@users.noreply.github.com>
2024-06-25 02:31:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function log(text){console.log(text);}
+document.body.style.backgroundImage = 'url(https://steamuserimages-a.akamaihd.net/ugc/914673891443473617/538580E91B95B582110030CD7C9438E95063AE50/?imw=512&amp;imh=288&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true)'
+//let main = document.body
+//main.innerHTML += "<h1 style='margin-top: 80px;  text-shadow: 2px 3px 2px rgba(203, 4, 216, 1); color: white ;font-weight: bold ; display: block' class='heroText'>★ All Mages ★ </h1><br> <hr>"
+
+
+
+
+
+
